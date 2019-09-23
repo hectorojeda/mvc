906fac6e5f32f5c1ed646eb7121c2eca9095fb24
hector <hectorojeda995@gmail.com>
2019-09-23 16:06:23 -0500

    verion 1

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+          public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+          public IActionResult Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid){
+                //lo que hay que hacer
+            }
+            return View();
+        }
+
+
+    }
+}
